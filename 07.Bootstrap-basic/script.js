c8f9a7721edeff8ca64999ac681ceedfb1f8c3ba
Muhammad Asad Tanveer <asadat2022@gmail.com>
2023-07-08 14:58:58 +0500

    BootStrap

---
+++ 07.Bootstrap-basic/script.js
@@ -0,0 +1,78 @@
+// console.log("external");
+
+// var, let, const
+
+// var $test;
+// var _test;
+// var test;
+// var first_name;
+// var firstName;
+// var first$name;
+// var first-name;
+
+
+
+// var first_name = "Ali";
+// var last_name = "Ahmad";
+// var first_name = "Hamza";
+
+// let f_name="test";
+// let f_name="second";
+
+// if (true) {
+//     let f_name="test";
+// }
+// console.log(f_name);
+
+// if (true) {
+//     var f_name="test";
+// }
+// console.log(f_name);
+
+// var first_name = "Ali";
+
+// const name = first_name;
+// console.log(name);
+// name = "sdhgjhd";
+// console.log(name);
+
+
+
+// var first_name;
+// first_name = "sdjhfhjds";
+// console.log(first_name);
+
+// Operaters
+// -> Arthematic Ope
+//      -> add +
+//      -> sub -
+//      -> div /
+//      -> mul *
+//      -> Mod %
+
+// -> Logical ope
+
+
+// -> Conditional ope
+// -> Asssignment Ope
+
+// if (2 === 2) {
+//     console.log(true);
+// }
+
+// console.log(2==="2" ? "equal":"not");
+
+
+
+var first_name = "Ali";
+
+first_name = [1,2,"Ali",4,5];
+
+first_name[3] = 20;
+first_name.push('Hamza');
+console.log(first_name);
+console.log(first_name.length);
+console.log(first_name.pop());
+console.log(first_name.length);
+
+console.log(first_name.join("*"));
