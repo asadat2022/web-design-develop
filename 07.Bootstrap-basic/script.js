d857eba591192ed5f2ce000166ecd5baad7b9593
Muhammad Asad Tanveer <asadat2022@gmail.com>
2023-07-12 14:14:00 +0500

    Object, Function

--- 07.Bootstrap-basic/script.js
+++ 07.Bootstrap-basic/script.js
@@ -66,13 +66,82 @@
 
 var first_name = "Ali";
 
-first_name = [1,2,"Ali",4,5];
+// first_name = [1,2,"Ali",4,5];
+first_name = new Array(1, 2, "Ali", 4, 5);
 
 first_name[3] = 20;
 first_name.push('Hamza');
-console.log(first_name);
-console.log(first_name.length);
-console.log(first_name.pop());
-console.log(first_name.length);
+// document.getElementById("test").innerText = first_name;
+// console.log(first_name);
+// console.log(first_name.length);
+// console.log(first_name.pop());
+// console.log(first_name.length);
 
-console.log(first_name.join("*"));
+// console.log(first_name.join("*"));
+
+
+// var std = ['Ali',"Ahmad","BA-3", "000-000000-0"]
+// var std = ['Ali',"Tanveer","Ahmad","BA-3", "000-000000-0"]
+
+
+
+// Array of Objects 
+var obj_std = [
+    { //Object 
+        name: {
+            first_name: "Ali",
+            mid_name: "Ahmad",
+            last_name: "Tanveer",
+        },
+        phone: [1, 2, 3],
+    },
+    {
+        name: {
+            first_name: "Raza",
+            mid_name: "Ahmad",
+            last_name: "Raza",
+        },
+        phone: [1, 2, 3],
+    }
+]
+
+
+// for (let index = 0; index < obj_std.length; index++) {
+//     const element = obj_std[index];
+//     console.log(element.name.last_name);
+// }
+
+// obj_std.forEach((element)=>{
+//     console.log(element.name.last_name);
+// })
+
+const listing = (element)=>{  // This is an arrow Function Printing Data 
+    console.log("ID: ",element.id);
+    console.log("Username: ",element.username);
+}
+// 
+users.forEach(listing) // Using preDefine for loop Function For Array Required Function As a Parameter
+// console.log(obj_std.phone[0]);
+// console.log(obj_std.name.last_name);
+
+
+// var obj_std = {
+//     first_name: "Madi",
+//     mid_name: "",
+//     last_name: "Tanveer",
+// }
+
+// console.log(obj_std.mid_name);
+
+
+// function sum(x,y) { //Function With Function keyword 
+//     console.log(x+y);
+// }
+
+const sum = (a, b) => { //Arrow Function For Sum 
+    console.log(a + b);
+};
+
+sum(4, 5);
+
+sum(7, 5);
