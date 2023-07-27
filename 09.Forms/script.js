20413b0da82014dca2052c416422d5bd113c6932
Muhammad Asad Tanveer <asadat2022@gmail.com>
2023-07-27 21:13:13 +0500

    Form (jQuery)

---
+++ 09.Forms/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function (){
+    $('#handleSubmit').submit((event)=>{
+        event.preventDefault();
+        // $('#label-name').text($('#field-name').val());
+        // $('#label-username').text($('#field-username').val());
+
+        // $('#label-hobby').text('')
+        // $('#label-hobby').append($('#hobby-cricket:checked').val()+", ")
+        // $('#label-hobby').append($('#hobby-gardening:checked').val()+", ")
+        // $('#label-hobby').append($('#hobby-reading:checked').val()+", ")
+
+        
+        // $('#label-gender').html($('.gender[name="gender"]:checked').val())
+
+        $('#field-name').val("sdjhfgbdsjh");
+    })
+});
