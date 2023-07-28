8a2f0eba80897fd462790213bcb040bd295f3390
Muhammad Asad Tanveer <asadat2022@gmail.com>
2023-07-28 17:24:39 +0500

    Form (jQuery)

--- 09.Forms/script.js
+++ 09.Forms/script.js
@@ -1,17 +1,26 @@
-$(document).ready(function (){
-    $('#handleSubmit').submit((event)=>{
-        event.preventDefault();
-        // $('#label-name').text($('#field-name').val());
-        // $('#label-username').text($('#field-username').val());
+$(document).ready(function () {
+    $('#handleSubmit').submit(function (event) {
+        if (!(this).checkValidity()) {
+            event.preventDefault()
+        } else {
+            $('#label-name').text($('#field-name').val());
+            $('#label-username').text($('#field-username').val());
 
-        // $('#label-hobby').text('')
-        // $('#label-hobby').append($('#hobby-cricket:checked').val()+", ")
-        // $('#label-hobby').append($('#hobby-gardening:checked').val()+", ")
-        // $('#label-hobby').append($('#hobby-reading:checked').val()+", ")
+            // $('#label-hobby').text('')
+            // $('#label-hobby').append($('#hobby-cricket:checked').val()+", ")
+            // $('#label-hobby').append($('#hobby-gardening:checked').val()+", ")
+            // $('#label-hobby').append($('#hobby-reading:checked').val()+", ")
 
-        
-        // $('#label-gender').html($('.gender[name="gender"]:checked').val())
 
-        $('#field-name').val("sdjhfgbdsjh");
+            // $('#label-gender').html($('.gender[name="gender"]:checked').val())
+
+            // $('#field-name').val("sdjhfgbdsjh");
+
+            event.preventDefault()
+        }
+
+        $(this).addClass('was-validated')
+
+
     })
 });
---
+++ 10.Bootstrap/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+
+    $('#alert').hide()
+    setTimeout(() => {
+        $('#alert').show()
+    }, 1000);
+});
